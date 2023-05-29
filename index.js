5d7a81905a04c32e180ddcd092ccfaf191e5a34a
vikas924 <obroy902@gmail.com>
2023-05-29 19:35:34 +0530

    Fix display and add function

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
+const arr = JSON.parse(localStorage.getItem('formData')) || [];
+
 const form = document.querySelector('#form');
 const add = document.getElementById('addBtn');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 
-form.addEventListener('add', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-form.addEventListener('click', () => {
+add.addEventListener('click', () => {
   const formData = {
     title: document.getElementById('title').value,
     author: document.getElementById('author').value,
   };
-  localStorage.setItem('formData', JSON.stringify(formData));
+  arr.push(formData)
+
+  localStorage.setItem('formData', JSON.stringify(arr));
 });
 
 function display() {
-  if (localStorage.getItem('formData')) {
-    const { title, author } = Json.parse(localStorage.getItem('formData'));
-    const output = document.getElementById('output');
-    output.innerHTML = `
-        <h2>${title}</h2> by <h2>${author}</h2>
+  const output = document.querySelector('#output');
+  let data = '';
+    for(let i = 0; i <= arr.length - 1; i += 1) {   
+     data += `<div>
+        <h2>${arr[i].title}</h2> by <h2>${arr[i].author}</h2>
         
-        <button id="removeBtn">remove</button>
+        <button id="removeBtn">remove</button></div>
         `;
-  }
-  form.addEventListener('click',  display());
+    }
+    output.innerHTML = data;
 }
+
+add.addEventListener('click', display);
+
+display();
