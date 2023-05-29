11df6568a5ce784703c8516b1893801e7e948a5f
github_username <email_address>
2023-05-29 17:39:38 +0300

    add remove and fix linter error

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const arr = JSON.parse(localStorage.getItem('formData')) || [];
 
 const form = document.querySelector('#form');
 const add = document.getElementById('addBtn');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+// const title = document.getElementById('title');
+// const author = document.getElementById('author');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -14,24 +14,40 @@ add.addEventListener('click', () => {
     title: document.getElementById('title').value,
     author: document.getElementById('author').value,
   };
-  arr.push(formData)
+  arr.push(formData);
 
   localStorage.setItem('formData', JSON.stringify(arr));
 });
 
+function remove() {
+  const parent = document.querySelector('#output');
+  const button = document.querySelectorAll('#removeBtn');
+  const removeBTN = Array.from(button).indexOf(this);
+  const RemoveDesire = this.parentNode;
+  parent.removeChild(RemoveDesire);
+  arr.splice(removeBTN, 1);
+  localStorage.setItem('formData', JSON.stringify(arr));
+}
+
 function display() {
   const output = document.querySelector('#output');
   let data = '';
-    for(let i = 0; i <= arr.length - 1; i += 1) {   
-     data += `<div>
+  for (let i = 0; i <= arr.length - 1; i += 1) {
+    data += `<div>
         <h2>${arr[i].title}</h2> by <h2>${arr[i].author}</h2>
         
         <button id="removeBtn">remove</button></div>
         `;
-    }
-    output.innerHTML = data;
+  }
+  output.innerHTML = data;
+  for (let i = 0; i < arr.length; i += 1) {
+    const button = document.querySelectorAll('#removeBtn');
+    button[i].addEventListener('click', remove);
+  }
 }
 
 add.addEventListener('click', display);
 
 display();
+
+// Remove //
