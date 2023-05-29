abb3de91d30f67dd9fcca1a3591aad9ca4bd5c4a
github_username <email_address>
2023-05-29 14:58:47 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const form = document.querySelector('#form')
-const add = document.querySelector('#addBtn')
-const title = document.querySelector('#title')
-const author = document.querySelector('#author')
+const form = document.querySelector('#form');
+const add = document.getElementById('addBtn');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+
+form.addEventListener('add', (e) => {
+  e.preventDefault();
+});
+
+form.addEventListener('click', () => {
+  const formData = {
+    title: document.getElementById('title').value,
+    author: document.getElementById('author').value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+function display() {
+  if (localStorage.getItem('formData')) {
+    const { title, author } = Json.parse(localStorage.getItem('formData'));
+    const output = document.getElementById('output');
+    output.innerHTML = `
+        <h2>${title}</h2><br>
+        <h2>${author}</h2>
+        <button id="removeBtn">remove</button>
+        `;
+  }
+
+  display();
+}
