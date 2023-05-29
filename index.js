c7e7103bbea2db551d178a3c94f047c96a7d8b04
github_username <email_address>
2023-05-29 14:03:03 +0300

    add form html style css

---
+++ index.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('#form')
+const add = document.querySelector('#addBtn')
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
